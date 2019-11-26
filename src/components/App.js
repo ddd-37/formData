7fd5cc4a78576a7e474841410d43d2d8e56a80d7
Devon Deason <devon.d.deason@gmail.com>
2019-11-26 07:31:38 -0700

    get axios running

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,15 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onFormSubmit = term => {
-    console.log(`onFormSubmit: ${term}`);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization:
+          "Client-ID 1f62b339e2d14f114aae723132d06d9a8f08acf9d81c7b46172d5c3ff79d31ef"
+      }
+    });
   };
   render() {
     return (
