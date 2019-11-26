7545a713ef2cec6e7775792bc4aaaec5c58b6018
Devon Deason <devon.d.deason@gmail.com>
2019-11-26 12:39:30 -0700

    setup image card

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = props => {
   console.log(props);
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img src={urls.regular} alt={description} key={id} />;
+  const images = props.images.map(image => {
+    return <ImageCard image={image} key={image.id} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
