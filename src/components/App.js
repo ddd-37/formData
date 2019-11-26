3da79fd6aa99491d60bd97c4fd1dd6171dd66f74
Devon Deason <devon.d.deason@gmail.com>
2019-11-26 07:42:05 -0700

    add some async goodness

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
+import { log } from "util";
 
 class App extends React.Component {
-  onFormSubmit = term => {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onFormSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: {
         query: term
       },
@@ -13,7 +14,10 @@ class App extends React.Component {
           "Client-ID 1f62b339e2d14f114aae723132d06d9a8f08acf9d81c7b46172d5c3ff79d31ef"
       }
     });
-  };
+
+    console.log("TCL: App -> response", response.data.results);
+  }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
